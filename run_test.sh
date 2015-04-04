37bd5288c2364f8b61684132025c889d3b7541a8
Tao Peng <ptpttt@gmail.com>
2015-04-04 13:43:56 +0200

    refactor run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-TEST=test
-LIP=lips
-
 success=0
 
 test_block () {
-    awk -f "./$LIP/block.awk" name="$1" "$TEST/block.input" | cmp "$TEST/block.$1.output"
+    awk -f "./lips/block.awk" name="$1" "test/block.input" | cmp "test/block.$1.output"
     test "$?" -eq 0 && let success+=1
 }
 
 test_it () {
-    awk -f "./$LIP/$1.awk" "$TEST/$1.input" | cmp "$TEST/$1.output"
+    awk -f "./lips/$1.awk" "test/$1.input" | cmp "test/$1.output"
     test "$?" -eq 0 && let success+=1
 }
 
